4c245c05057aeaa177bcf987c3459a8cb3861cad
ShilpiRathaur04-code <rathaurshilpi162@gmail.com>
2026-08-03 22:21:40 +0530

    Time: 0 ms (100%), Space: 23.8 MB (83.38%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n= nums.size();
+
+        //brute force
+        // vector<int>temp;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i] != 0){
+        //         temp.push_back(nums[i]);
+        //     }
+        // }
+        // for(int i=0;i<temp.size();i++){
+        //     nums[i]=temp[i];
+
+        // }
+        // for(int i=temp.size();i<n;i++){
+        //     nums[i] = 0;
+        // }
+
+        //optimize sol 
+        int j=-1;
+        for(int i=0;i<n;i++){
+            if(nums[i] == 0){
+                j=i;
+                break;
+            }
+        }
+        if(j==-1){
+            return;
+        }
+       
+        for(int i=j+1;i<n;i++){
+            if(nums[i]!=0){
+                swap(nums[i] ,nums[j]);
+                j++;
+            }
+        }
+        
+    }
+};
