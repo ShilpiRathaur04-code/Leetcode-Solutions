9a525c88f527b16e41fe5bfbe6cf29b104494c22
ShilpiRathaur04-code <rathaurshilpi162@gmail.com>
2026-08-11 12:03:56 +0530

    Time: 7 ms (11.15%), Space: 28.2 MB (74.23%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -36,16 +36,37 @@ public:
 
         //  optimaized
 
-        map<int,int>mp;
+        // map<int,int>mp;
+        // int n= nums.size();
+        // for(int i=0;i<n;i++){
+        //     mp[nums[i]]++;
+        // }
+        // for(auto i:mp){
+        //     if(i.second > nums.size()/2){
+        //         return i.first;
+        //     }
+        // }
+        // return -1;
+        sort(nums.begin(), nums.end());
         int n= nums.size();
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;
-        }
-        for(auto i:mp){
-            if(i.second > nums.size()/2){
-                return i.first;
+        int count =1,maxi=1;
+        int ans=nums[0];
+        for(int i=1;i<n;i++){
+            if(nums[i] == nums[i-1]){
+                count ++;
+            }else{
+                count=1;
+            }
+            if(count>maxi){
+                maxi=count;
+                ans = nums[i];
             }
         }
-        return -1;
-    }             
+        return ans;
+
+         
+    
+    
+    
+    }
 };
